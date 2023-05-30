078823337dde67663d0c46d85a76046b57be790e
Ehab <154797@student.horizoncollege.nl>
2023-05-30 10:52:14 +0200

    New setup test code nocodemie

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,56 @@
-// playwright.config.ts
-import { PlaywrightTestConfig } from '@playwright/test';
-import { appSettings } from './app-settings';
-import { defineConfig } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from "@playwright/test";
 
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 const config: PlaywrightTestConfig = {
-  timeout: 5 * 60 * 1000,
-  globalSetup: require.resolve('./global-setup'),
-  use: {
-    // Tell all tests to load signed-in state from 'storageState.json'.
-    storageState: `./local-storage/${appSettings.app_identifier}.json`,
+  testDir: "./tests",
+
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
   },
+
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+
+  /* Opt out of parallel tests on CI. */
+  workers: 1, //process.env.CI ? 1 : undefined,
+
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "json",
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+
+      /* Project-specific settings. */
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
 };
 export default config;
-
-
--- tests/nocodemie.spec.ts
+++ tests/nocodemie.spec.ts
@@ -1,122 +1,57 @@
-import test, { expect } from '@playwright/test';
-import { Chance } from 'chance';
-import { ExamUtils } from '../page-models/test';
-import { appSettings } from '../app-settings';
-import { url } from '../app-settings.json';
+import { test, expect } from "@playwright/test";
+import { url } from "../app-settings.json";
 
-const chance = new Chance();
-const newPageTitle = "Home";
+const newPageTitle = "home";
 
 test.beforeEach(async ({ page }) => {
   const newPageUrl = url + "/" + newPageTitle.toLocaleLowerCase();
   await page.goto(newPageUrl);
 });
 
-test.describe('case management', async () => {
-    test('1.1 Create a new blank page and name it ${newPageTitle}', async ({
-      page,
-    }) => {
-      const loginUtils = new ExamUtils(page);
-      // // Betty Blocks inloggen
-      // // await page.goto('https://my.bettyblocks.com/');
-      // await page.type('input[name="loginId"]', appSettings.bettyuser.username);
-      // // await page.keyboard.press('Enter');
-      // await page.type('input[name="password"]', appSettings.bettyuser.password);
-      // await page.keyboard.press('Enter');
-      // await page.waitForTimeout(3000);
-      // const caseoverviewUtils = new ExamUtils(page);
-      // await caseoverviewUtils.goto(page, '/home');
-      // await page.waitForTimeout(3000);
-      // expect(page.url()).toMatch('/home');
+test(`1.1 Create a new blank page and name it ${newPageTitle}`, async ({
+  page,
+}) => {
+  const rootElement = page.locator("id=app");
+  await expect(rootElement, "page is create and compiled").toBeDefined();
+  await page.waitForTimeout(2000);
+});
 
-      const rootElement = page.locator("id=app");
-      await expect(rootElement, "page is create and compiled").toBeDefined();
-    });
+test(`1.2 Drag a ‘1 Column’ component onto your page`, async ({ page }) => {
+  const row = await page.locator(`[data-component=Row]`).nth(0);
+  await expect(row, "row should be in document").toBeVisible();
 
-    test('1.2 Drag a ‘1 Column’ component onto your page', async ({
-      page,
-    }) => {
-      const loginUtils = new ExamUtils(page);
-     
-      // // Betty Blocks inloggen
-      // // await page.goto('https://my.bettyblocks.com/');
-      // await page.type('input[name="loginId"]', appSettings.bettyuser.username);
-      // // await page.keyboard.press('Enter');
-      // await page.type('input[name="password"]', appSettings.bettyuser.password);
-      // await page.keyboard.press('Enter');
-      // await page.waitForTimeout(3000);
-      // const caseoverviewUtils = new ExamUtils(page);
-      // await caseoverviewUtils.goto(page, '/home');
-      // await page.waitForTimeout(3000);
-      // expect(page.url()).toMatch('/home');
+  await expect(
+    row.locator(`[data-component=Column]`).nth(0),
+    "column should be in document"
+  ).toBeVisible();
+  await page.waitForTimeout(2000);
+});
 
-      const row = await page.locator(`[data-component=Row]`).nth(0);
-      await expect(row, "row should be in document").toBeVisible();
+test(`1.3 Drag a ‘Text’ component into your ‘1 Column’ component`, async ({
+  page,
+}) => {
+  const column = page.locator(`[data-component=Column]`).nth(0);
+  await expect(column, "column should be in document").toBeVisible();
 
-      await expect(
-        row.locator(`[data-component=Column]`).nth(0),
-        "column should be in document"
-      ).toBeVisible();
-      
-      await page.waitForTimeout(4000);
-    });
+  await expect(
+    column.locator(`[data-component=Text]`).nth(0),
+    "text should be in document"
+  ).toBeVisible();
+  await page.waitForTimeout(2000);
+});
 
-    test('1.3 Drag a ‘Text’ component into your ‘1 Column’ component', async ({
-      page,
-    }) => {
-      const loginUtils = new ExamUtils(page);
-     
-      // // Betty Blocks inloggen
-      // // await page.goto('https://my.bettyblocks.com/');
-      // await page.type('input[name="loginId"]', appSettings.bettyuser.username);
-      // // await page.keyboard.press('Enter');
-      // await page.type('input[name="password"]', appSettings.bettyuser.password);
-      // await page.keyboard.press('Enter');
-      // await page.waitForTimeout(3000);
-      // const caseoverviewUtils = new ExamUtils(page);
-      // await caseoverviewUtils.goto(page, '/home');
-      // await page.waitForTimeout(3000);
-      // expect(page.url()).toMatch('/home');
+const textContent = `My first page`;
 
-      const column = page.locator(`[data-component=Column]`).nth(0);
-      await expect(column, "column should be in document").toBeVisible();
-    
-      await expect(
-        column.locator(`[data-component=Text]`).nth(0),
-        "text should be in document"
-      ).toBeVisible();
+test(`1.4 Have the ‘Text’ component display the following text: ${textContent}`, async ({
+  page,
+}) => {
+  const column = page.locator(`[data-component=Column]`).nth(0);
+  await expect(column, "coulmn should be in document").toBeVisible();
+  const text = await column
+    .locator(`[data-component=Text]`)
+    .nth(0)
+    .textContent();
 
-      await page.waitForTimeout(4000);
-    });
-
-    const textContent = `My first page`;
-
-    test('1.4 Have the ‘Text’ component display the following text: ${textContent}', async ({
-      page,
-    }) => {
-      const loginUtils = new ExamUtils(page);
-     
-      // // Betty Blocks inloggen
-      // // await page.goto('https://my.bettyblocks.com/');
-      // await page.type('input[name="loginId"]', appSettings.bettyuser.username);
-      // // await page.keyboard.press('Enter');
-      // await page.type('input[name="password"]', appSettings.bettyuser.password);
-      // await page.keyboard.press('Enter');
-      // await page.waitForTimeout(3000);
-      // const caseoverviewUtils = new ExamUtils(page);
-      // await caseoverviewUtils.goto(page, '/home');
-      // await page.waitForTimeout(3000);
-      // expect(page.url()).toMatch('/home');
-
-      const column = page.locator(`[data-component=Column]`).nth(0);
-      await expect(column, "coulmn should be in document").toBeVisible();
-      const text = await column
-        .locator(`[data-component=Text]`)
-        .nth(0)
-        .textContent();
-    
-      await expect(text, `text should be: `).toBe(textContent);
-
-      await page.waitForTimeout(4000);
-    });
+  await expect(text, `text should be: `).toBe(textContent);
+  await page.waitForTimeout(2000);
 });
