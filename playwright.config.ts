0cfe60e8c186b20fccf2ad3e5d907f1c8fcebcdc
Ehab <56022276+154797@users.noreply.github.com>
2023-06-01 15:21:21 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ const config: PlaywrightTestConfig = {
   workers: 1, //process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [[‘json’, { outputFile: result-${{ github.run_number }}.json }]],
+  reporter: [[‘json’, { outputFile: 'result-${{ github.run_number }}.json' }]],
   
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
